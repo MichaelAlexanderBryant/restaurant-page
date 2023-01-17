f1f1d14815f618e64c9c79e7465065f8a340963c
MichaelABryant <m.alexander.bryant@gmail.com>
2023-01-17 13:41:43 -0800

    Create tabs for home contact menu pages

--- src/website.js
+++ src/website.js
@@ -11,8 +11,20 @@ function addHeader() {
 function addNavigation() {
     const nav = document.createElement("div");
     const home = document.createElement("span");
+    home.addEventListener("click", () => {
+        document.getElementById("content").textContent = '';
+        loadHomepage();
+    });
     const contact = document.createElement("span");
+    contact.addEventListener("click", () => {
+        document.getElementById("content").textContent = '';
+        loadContact();
+    });
     const menu = document.createElement("span");
+    menu.addEventListener("click", () => {
+        document.getElementById("content").textContent = '';
+        loadMenu();
+    });
     home.textContent = "Home"
     contact.textContent = "Contact"
     menu.textContent = "Menu"
